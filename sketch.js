7ba3c19efc40cd2833566b7e32e1547d4e6a93bf
Donica Gheorghe <gheorghedonica@gmail.com>
2021-08-13 13:32:34 +0300

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var canvasWidth = 600;
+var canvasHeight = 400;
+
+blocks = ['0', '10', '15'];
+
+
+function preload(){
+
+}
+
+function setup() {
+  var canvas =  createCanvas(canvasWidth, canvasHeight);
+  canvas.parent('sketch-holder')
+  frameRate(30);
+
+}
+
+function draw() {
+  rect(50, 50, 50, 50);
+  background(0)
+  fill(0);
+}
